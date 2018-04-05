959e7ce145f984d83a8813a84eb1b5c04657cb70
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2018-04-05 11:57:05 +0200

    Update to tabOTTR 0.2. Support list values.

--- lutra-tab/src/main/java/xyz/lutra/tabottr/TabOTTR.java
+++ lutra-tab/src/main/java/xyz/lutra/tabottr/TabOTTR.java
@@ -12,7 +12,9 @@ public abstract class TabOTTR {
 	public static final String TYPE_BLANK = "blank";
 	public static final String TYPE_TEXT = "text";
 	public static final String TYPE_AUTO = "auto";
+	public static final String TYPE_LIST_POSTFIX = "+";
 	
+	public static final String VALUE_LIST_SEPARATOR = "|";
 	public static final String VALUE_FRESH_BLANK = "*";
 	public static final String VALUE_BLANK_NODE_PREFIX = "_:";
 	public static final String VALUE_LANGUAGE_TAG_PREFIX = "@@";
--- lutra-tab/src/test/java/xyz/lutra/tabottr/io/rdf/PrototypeTest.java
+++ lutra-tab/src/test/java/xyz/lutra/tabottr/io/rdf/PrototypeTest.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.jena.rdf.model.Model;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.Test;
 
+import osl.util.rdf.ModelIO;
 import osl.util.rdf.ModelIOException;
+import xyz.lutra.Expander;
 import xyz.lutra.tabottr.Table;
 import xyz.lutra.tabottr.io.TableReader;
 import xyz.lutra.tabottr.io.excel.ExcelReader;
@@ -17,7 +21,7 @@ public class PrototypeTest {
 	
 	private String ROOT = "src/test/resources/";
 			
-	public void shouldWork() throws IOException, ModelIOException, InvalidFormatException {
+	@Test public void shouldWork() throws IOException, ModelIOException, InvalidFormatException {
 		TableReader parser = new ExcelReader(ROOT + "test1.xlsx");
 		List<Table> tables = parser.getTables();
 		
@@ -27,10 +31,10 @@ public class PrototypeTest {
 		
 		RDFWriter writer = new RDFWriter();
 		writer.process(tables);
-		//Model instances = writer.write();
+		Model instances = writer.write();
 		//ModelIO.printModel(instances, ModelIO.format.TURTLE);
 		
-		//Model expansion = Expander.expand(instances);
+		Model expansion = Expander.expand(instances);
 		//ModelIO.printModel(expansion, ModelIO.format.TURTLE);
 		assertTrue(true);
 	}
