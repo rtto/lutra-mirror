8da45db6f7b0baced48ee8f45debdd3aad2e7a16
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2019-03-24 22:48:32 +0100

    Initial testing of new indented RDF Writer. #77
    Needs more testing. Fails when running CLI.

--- lutra-wottr/src/main/java/xyz/ottr/lutra/wottr/util/ModelIO.java
+++ lutra-wottr/src/main/java/xyz/ottr/lutra/wottr/util/ModelIO.java
@@ -22,43 +22,85 @@ package xyz.ottr.lutra.wottr.util;
  * #L%
  */
 
-import java.io.IOException;
-import java.io.StringWriter;
+import io.bdrc.jena.sttl.CompareComplex;
+import io.bdrc.jena.sttl.ComparePredicates;
+import io.bdrc.jena.sttl.STTLWriter;
+
 import java.util.List;
+import java.util.SortedMap;
 import java.util.StringJoiner;
 
 import org.apache.jena.graph.Node;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.RDFList;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFWriter;
+import org.apache.jena.sparql.util.Context;
+import org.apache.jena.sparql.util.Symbol;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.util.FileUtils;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.XSD;
+
+import xyz.ottr.lutra.OTTR;
+import xyz.ottr.lutra.wottr.WOTTR;
 
 public abstract class ModelIO {
-
-    public enum Format {
-        RDFXML(FileUtils.langXMLAbbrev), 
-        TURTLE(FileUtils.langTurtle), 
-        N3(FileUtils.langN3), 
-        NTRIPLES(FileUtils.langNTriple);
+    
+    private static Context ctx;
+    private static Lang sttl;
+    
+    static {
+        // See https://github.com/buda-base/jena-stable-turtle:
+        // register the STTL writer
+        sttl = STTLWriter.registerWriter();
+        // build a map of namespace priorities
+        SortedMap<String, Integer> nsPrio = ComparePredicates.getDefaultNSPriorities();
+        nsPrio.put(OWL.getURI(), 1);
+        nsPrio.put(XSD.getURI(), 1);
+        nsPrio.put(OTTR.namespace, 2);
         
-        private final String lang;
-
-        private Format(final String lang) {
-            this.lang = lang;
-        }
+        // build a list of predicates URIs to be used (in order) for blank node comparison
+        List<String> predicatesPrio = CompareComplex.getDefaultPropUris();
+        predicatesPrio.add(WOTTR.values.getURI());
+        predicatesPrio.add(WOTTR.parameters.getURI());
+        
+        // pass the values through a Context object
+        ctx = new Context();
+        ctx.set(Symbol.create(STTLWriter.SYMBOLS_NS + "nsPriorities"), nsPrio);
+        ctx.set(Symbol.create(STTLWriter.SYMBOLS_NS + "nsDefaultPriority"), 2);
+        ctx.set(Symbol.create(STTLWriter.SYMBOLS_NS + "complexPredicatesPriorities"), predicatesPrio);
+        // the base indentation, defaults to 4
+        ctx.set(Symbol.create(STTLWriter.SYMBOLS_NS + "nsBaseIndent"), 4);
+        // the minimal predicate width, defaults to 14
+        ctx.set(Symbol.create(STTLWriter.SYMBOLS_NS + "predicateBaseWidth"), 14);
     }
 
+    public enum Format {
+        RDFXML("RDF/XML-ABBREV", Lang.RDFXML), 
+        TURTLE("TURTLE", sttl), 
+        NTRIPLES("NT", Lang.NTRIPLES);
+        
+        private final String readFormat;
+        private final Lang writeLang;
+
+        private Format(final String readFormat, final Lang writeLang) {
+            this.readFormat = readFormat;
+            this.writeLang = writeLang;
+        }
+    }
+    
     public static void printModel(Model model, ModelIO.Format format) throws ModelIOException {
         System.out.println(writeModel(model, format));
     }
 
     public static Model readModel(String file) {
-        return readModel(file, FileUtils.guessLang(file, ModelIO.Format.TURTLE.lang));
+        return readModel(file, FileUtils.guessLang(file, ModelIO.Format.TURTLE.readFormat));
     }
 
     public static Model readModel(String file, ModelIO.Format serialisation) {
-        return readModel(file, serialisation.lang);
+        return readModel(file, serialisation.readFormat);
     }
 
     private static Model readModel(String file, String serialisation) {
@@ -98,16 +140,12 @@ public abstract class ModelIO {
     }
 
     private static String writeRDFModel(Model model, ModelIO.Format format) {
-        StringWriter str = new StringWriter();
-        model.write(str, format.lang);
-        String modelString = str.toString();
-        str.flush();
-        try {
-            str.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return modelString;
+        RDFWriter writer = RDFWriter.create()
+                .source(model.getGraph())
+                .context(ctx)
+                .lang(format.writeLang)
+                .build();
+        return writer.asString();
     }
 
 }
