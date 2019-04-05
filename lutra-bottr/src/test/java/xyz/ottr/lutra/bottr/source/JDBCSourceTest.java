fe8d04e8509f4e41049d8a0d4801c11027383dde
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2019-04-05 22:11:54 +0200

    Drop H2 test table only *if it exists*

--- lutra-bottr/src/test/java/xyz/ottr/lutra/bottr/source/JDBCSourceTest.java
+++ lutra-bottr/src/test/java/xyz/ottr/lutra/bottr/source/JDBCSourceTest.java
@@ -52,7 +52,7 @@ public class JDBCSourceTest {
         Statement stmt = conn.createStatement();
         
         //Create table
-        stmt.execute("DROP TABLE CUSTOMER");
+        stmt.execute("DROP TABLE IF EXISTS CUSTOMER");
         stmt.execute("CREATE TABLE CUSTOMER (id number, name varchar(20), age number, address varchar(20), salary number);");
         stmt.execute("INSERT into CUSTOMER values (1, 'Paulo', 32, 'Niteroi', 2500);");
         stmt.execute("INSERT into CUSTOMER values (2, 'Pedro', 33, 'Porto Alegre', 2700);");
