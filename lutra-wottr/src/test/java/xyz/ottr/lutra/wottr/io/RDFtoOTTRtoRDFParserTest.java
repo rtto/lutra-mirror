6b633083ed97f492476f6d7975470952dbaba3cc
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2019-05-01 21:29:37 +0200

    Fix code style errors.

--- lutra-wottr/src/test/java/xyz/ottr/lutra/wottr/io/RDFtoOTTRtoRDFParserTest.java
+++ lutra-wottr/src/test/java/xyz/ottr/lutra/wottr/io/RDFtoOTTRtoRDFParserTest.java
@@ -1,6 +1,30 @@
 package xyz.ottr.lutra.wottr.io;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/*-
+ * #%L
+ * lutra-wottr
+ * %%
+ * Copyright (C) 2018 - 2019 University of Oslo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import static org.junit.Assume.assumeNotNull;
 
 import java.io.IOException;
@@ -27,7 +51,7 @@ import xyz.ottr.lutra.wottr.WTemplateFactory;
 import xyz.ottr.lutra.wottr.util.ModelIO;
 
 @RunWith(Parameterized.class)
-public class RDFOTTRParserTest {
+public class RDFtoOTTRtoRDFParserTest {
 
     @Parameters(name = "{index}: {0}")
     public static List<String> data() throws IOException {
@@ -42,7 +66,7 @@ public class RDFOTTRParserTest {
 
     private String filename;
 
-    public RDFOTTRParserTest(String filename) {
+    public RDFtoOTTRtoRDFParserTest(String filename) {
         this.filename = filename;
     }
 
