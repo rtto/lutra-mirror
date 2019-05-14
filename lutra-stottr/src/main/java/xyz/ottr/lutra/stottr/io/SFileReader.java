26b7430a8e8a51a040098fcb951f1af75588a832
Leif Harald Karlsen <leifhka@ifi.uio.no>
2019-05-14 17:05:08 +0200

    Add file reader for stOTTR.

---
+++ lutra-stottr/src/main/java/xyz/ottr/lutra/stottr/io/SFileReader.java
@@ -0,0 +1,45 @@
+package xyz.ottr.lutra.stottr.io;
+
+/*-
+ * #%L
+ * lutra-stottr
+ * %%
+ * Copyright (C) 2018 - 2019 University of Oslo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
+import xyz.ottr.lutra.io.InputReader;
+import xyz.ottr.lutra.result.Message;
+import xyz.ottr.lutra.result.Result;
+import xyz.ottr.lutra.result.ResultStream;
+
+public class SFileReader implements InputReader<String, CharStream> {
+
+    public ResultStream<CharStream> apply(String filename) {
+
+        try {
+            return ResultStream.innerOf(CharStreams.fromFileName(filename));
+        } catch (IOException ex) {
+            return ResultStream.of(Result.empty(Message.error(ex.getMessage())));
+        }
+    }
+}
