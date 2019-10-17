4ab014d6787e4dbe4ce615039557b9355f57d3b8
Leif Harald Karlsen <leifhka@ifi.uio.no>
2019-10-17 17:15:00 +0200

    Adapt output of --help to recent changes, and state that each library must have files of the same format, and that without an explicit format set, all available formats are attempted for each library.

--- lutra-cli/src/main/java/xyz/ottr/lutra/cli/Settings.java
+++ lutra-cli/src/main/java/xyz/ottr/lutra/cli/Settings.java
@@ -48,9 +48,9 @@ import xyz.ottr.lutra.result.Message;
         + "The following translates all template files (with .ttl-extension) in ./lib from the legacy format to wottr,"
         + " and writes them to ./wottr:%n%n"
         + "    lutra -L legacy -O wottr -m formatLibrary -l ./lib -o ./wottr%n%n"
-        + "The following expands all instances in ins1.xlsx and ins2.xlsx in tabOTTR using the templates in ./lib and writes"
-        + " the expanded instances to exp.ttl in the wOTTR format:%n%n"
-        + "    lutra -I tabottr -O wottr -m expand -l ./lib -o exp.ttl ins1.xlsx ins2.xlsx"
+        + "The following expands all instances in ins1.xlsx and ins2.xlsx in tabOTTR using the templates"
+        + " in ./baselib and ./domain and writes the expanded instances to exp.ttl in the wOTTR format:%n%n"
+        + "    lutra -I tabottr -O wottr -m expand -l ./baselib -l ./domain -o exp.ttl ins1.xlsx ins2.xlsx"
         + "%n%n@|bold DISCUSSION:|@%n"
         + "Note that with -O wottr all triple-instances outside of template definitions are written as normal RDF triples,"
         + " thus to expand a set of instances into an RDF graph this is what should be used."
@@ -60,7 +60,11 @@ import xyz.ottr.lutra.result.Message;
         + " E.g. with -o ./templates, the template with IRI"
         + "%n    http://example.org/draft/owl/SubclassOf%n"
         + "will be written to the path"
-        + "%n    ./templates/draft/owl/SubclassOf.ttl."
+        + "%n    ./templates/draft/owl/SubclassOf.ttl.%n%n"
+        + "Note that one can omit giving a format for libraries. In this case all possible formats are attempted,"
+        + " and the first to succeed for each library is used for that library. However, all files within one"
+        + " library needs to be of the same format, but"
+        + " different libraries can have files of different formats."
         + "%n%n@|bold FURTHER INFORMATION:|@%n"
         + "Website: https://ottr.xyz%n"
         + "Source:  https://gitlab.com/ottr/lutra/lutra"
@@ -96,7 +100,7 @@ public class Settings {
     public Format outputFormat = Format.wottr;
 
     @Option(names = {"-L", "--libraryFormat"}, completionCandidates = TplInputFormat.class,
-        description = {"The input format of the library.%n"
+        description = {"The input format of the libraries. If omitted, all available formats are attempted.%n"
                        + "(legal values: ${COMPLETION-CANDIDATES})"})
     public Format libraryFormat;
 
@@ -112,7 +116,9 @@ public class Settings {
         description = {"The input format of the templates fetched via the -f flag."})
     public Format fetchFormat;
 
-    @Option(names = {"-l", "--library"}, description = {"Folder containing templates to use as library."})
+    @Option(names = {"-l", "--library"}, 
+        description = {"Folder containing templates to use as library."
+                       + " Can be used multiple times for multiple libraries."})
     public String[] library;
 
     @Parameters(description = {"Files of instances to which operations are to be applied."})
