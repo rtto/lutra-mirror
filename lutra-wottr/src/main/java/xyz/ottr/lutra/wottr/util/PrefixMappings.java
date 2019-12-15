596fc1093380957d267b36daae47f472953beb27
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2019-12-15 14:33:19 +0100

    Minor fixes. [skip ci]

--- lutra-wottr/src/main/java/xyz/ottr/lutra/wottr/util/PrefixMappings.java
+++ lutra-wottr/src/main/java/xyz/ottr/lutra/wottr/util/PrefixMappings.java
@@ -27,6 +27,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.shared.PrefixMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -131,13 +134,15 @@ public enum PrefixMappings {
         }
     }
 
+    // TODO This might be too heavy for big data, should be possible to disable.
     private static Set<String> getAllURIsNamespaces(Model model) {
         return model.listObjects()
             .andThen(model.listSubjects())
-            .andThen(model.listStatements().mapWith(s -> s.getPredicate()))
-            .filterKeep(t -> t.isURIResource())
-            .mapWith(o -> o.asResource())
-            .mapWith(o -> o.getNameSpace())
+            .andThen(model.listStatements()
+                .mapWith(Statement::getPredicate))
+            .filterKeep(RDFNode::isURIResource)
+            .mapWith(RDFNode::asResource)
+            .mapWith(Resource::getNameSpace)
             .toSet();
     }
     
