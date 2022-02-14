2c8a2139eef96272cba493018a5dfc53856c14ad
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2022-02-14 17:11:36 +0100

    unit test: poor error message.
    #334

---
+++ lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issues334Test.java
@@ -0,0 +1,35 @@
+package xyz.ottr.lutra.cli;
+
+/*-
+ * #%L
+ * xyz.ottr.lutra:lutra-cli
+ * %%
+ * Copyright (C) 2018 - 2021 University of Oslo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import org.junit.Test;
+
+public class Issues334Test {
+    
+    private static final String ROOT = "src/test/resources/issues/334/";
+
+    @Test
+    public void testWOTTRBadArguments1() {
+        CLIRunner.run("-f --debugStackTrace " + ROOT + "instance1.wottr");
+    }
+}
