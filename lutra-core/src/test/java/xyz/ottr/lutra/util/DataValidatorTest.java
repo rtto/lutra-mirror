1ad87c38e417ba35acbe1fa7dc424958af8a38e7
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-03-19 10:51:37 +0100

    Add new test cases #343

--- lutra-core/src/test/java/xyz/ottr/lutra/util/DataValidatorTest.java
+++ lutra-core/src/test/java/xyz/ottr/lutra/util/DataValidatorTest.java
@@ -22,12 +22,16 @@ package xyz.ottr.lutra.util;
  * #L%
  */
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.util.function.Predicate;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.ottr.lutra.system.Assertions;
+import xyz.ottr.lutra.system.Message;
+import xyz.ottr.lutra.system.Result;
 
 public class DataValidatorTest {
     
@@ -47,43 +51,104 @@ public class DataValidatorTest {
     
     @Test
     public void shouldAcceptIntegers() {
-        for (String value : new String[] { "1", "-1234", "00000", "0", "91234" }) {
+        for (String value : new String[] {"0", "1", "00000", "91234", "007", "-123456789012345678901234567890" }) {
             accept(DataValidator::isInteger, value);
         }
+
+        accept(DataValidator::isInteger, " 43"); // fail
+        accept(DataValidator::isInteger, "43 "); // fail
+        accept(DataValidator::isInteger, "   43  "); // fail
+
+        //accept(DataValidator::isInteger, "+43"); // fail, not implemented
     }
-    
+
     @Test
     public void shouldRejectIntegers() {
-        for (String value : new String[] { "", "1.1", "asdf", "--123", "1-2", "12-" }) {
+        for (String value : new String[] { "", "  ", "1.1", "asdf", "--123", "1-2", "12-", "+ 43", "4 3" }) {
             reject(DataValidator::isInteger, value);
         }
     }
-    
+
     @Test
     public void shouldAcceptDecimals() {
         for (String value : new String[] { "1.0", "-1.1", "0.2", "-0.4", "91234.123" }) {
             accept(DataValidator::isDecimal, value);
         }
+
+        accept(DataValidator::isDecimal, "1.0 "); // fail
+        accept(DataValidator::isDecimal, " 1.0"); // fail
+        accept(DataValidator::isDecimal, " -1.0 "); // fail
+
+        //accept(DataValidator::isDecimal, "+12.456"); // fail, not implemented
+        //accept(DataValidator::isDecimal, " -.456"); // fail, not implemented
+        //accept(DataValidator::isDecimal, " -456."); // fail, not implemented
     }
     
     @Test
     public void shouldRejectDecimals() {
-        for (String value : new String[] { "", "1.1-", "asdf", "--123", "1-2", "12-", "1" }) {
+        for (String value : new String[] { "", "1.1-", "asdf", "--123", "1-2", "12-", "1" ,"-54", "-4.6.8", "@", "+ 1234.456", "1 234.456", "1234.456E+2" }) {
             reject(DataValidator::isDecimal, value);
         }
     }
-    
+
+    // valid char should give no error
+    @Test
+    public void asCharValidTest() {
+        Result<Character> c;
+        for (String value : new String[] { "v", "-" ,"4", "\n", "\0", " "}) {
+            c = DataValidator.asChar(value);
+            Assertions.noErrors(c); // fail
+        }
+    }
+
+    // invalid input should give error
+    @Test
+    public void asCharInvalidTest() {
+        Result<Character> c;
+        for (String value : new String[] {"abc", "--" ,"49"}) {
+            c = DataValidator.asChar(value);
+            assertSame(Message.Severity.ERROR, c.getMessageHandler().getMostSevere()); // fail
+        }
+    }
+
+    @Test
+    public void shouldAcceptChar() {
+        for (String value : new String[] { "-", "Æ" ,"4", "\n", "\0", " "}) {
+            accept(DataValidator::isChar, value);
+        }
+    }
+
+    @Test
+    public void shouldRejectChar() {
+        for (String value : new String[] { "no", "en-GB", "234", "", "  "}) {
+            reject(DataValidator::isChar, value);
+        }
+    }
+
     @Test
     public void shouldAcceptLanguageTag() {
-        for (String value : new String[] { "no", "en-GB", "asdf", "aa"}) {
+        for (String value : new String[] { "no", "en-GB", "en-GB-London"}) {
             accept(DataValidator::isLanguageTag, value);
         }
+
+        accept(DataValidator::isLanguageTag, "en-GB "); // fail
     }
     
     @Test
     public void shouldRejectLanguageTag() {
-        for (String value : new String[] { "", ".com", "@", "--123", "1-2", "12-", "1", "a a", " ", "91234.123"}) {
+        for (String value : new String[] { "", ".com", "@", "--123", "1-2", "12-", "1", "a a", "  ", "91234.123", "æøå", null}) {
             reject(DataValidator::isLanguageTag, value);
+        }
+
+        reject(DataValidator::isLanguageTag, "no-"); // fail
+    }
+
+    // should give warning/error? if the input is invalid
+    @Test
+    public void asLanguageTagStringTest() {
+        for (String value : new String[] { "abcd", "-en-GB", "234", "", ".com", "no-", null}) {
+            Result<String> str = DataValidator.asLanguageTagString(value);
+            assertSame(Message.Severity.WARNING, str.getMessageHandler().getMostSevere());
         }
     }
 }
