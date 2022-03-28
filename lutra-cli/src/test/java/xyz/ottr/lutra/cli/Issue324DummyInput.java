2069bdb43a7e8a8a772724d44bfd93941eddcb63
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-03-28 20:38:33 +0200

    Test parse() on empty/dummy/faulty/correct file

---
+++ lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issue324DummyInput.java
@@ -0,0 +1,50 @@
+package xyz.ottr.lutra.cli;
+
+import org.junit.Test;
+import xyz.ottr.lutra.system.MessageHandler;
+import xyz.ottr.lutra.wottr.io.RDFIO;
+
+public class Issue324DummyInput {
+
+    private static final String ROOT = "src/test/resources/issues/324DummyInput/";
+
+    @Test
+    public void parseDummyFile() {
+        /*
+        String args = " "
+                + " --debugStackTrace"
+                + " -l " + ROOT + "dummy.ttl"
+                + " " + ROOT + "dummy.ttl";
+
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        int exitCode = cli.executeArgs(args.trim().split("\\s+"));
+
+        //assertNotEquals(0, msgs.getMessages().size());
+        //assertEquals(Message.Severity.ERROR, msgs.getMostSevere());
+        //assertNotEquals("Exit code should not be 0 with error messages", 0, exitCode);
+         */
+
+        var result = RDFIO.fileReader().parse(ROOT + "dummy.ttl");
+        System.out.println(result);
+    }
+
+    @Test
+    public void parseFaultyFile() {
+        var result = RDFIO.fileReader().parse(ROOT + "faultyRDF.ttl");
+        System.out.println(result);
+    }
+
+
+    @Test
+    public void parseEmptyFile() {
+        var result = RDFIO.fileReader().parse(ROOT + "empty.ttl");
+        System.out.println(result);
+    }
+
+    @Test
+    public void parseCorrectFile() {
+        var result = RDFIO.fileReader().parse(ROOT + "correct.ttl");
+        System.out.println(result);
+    }
+}
