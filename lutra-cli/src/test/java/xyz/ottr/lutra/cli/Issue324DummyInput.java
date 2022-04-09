51f1d61092932d412772f510801763587e551e59
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-04-09 23:26:16 +0200

    Catch error messages in RDFReader::parse before they get lost in Result::mapToStream #324

--- lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issue324DummyInput.java
+++ lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issue324DummyInput.java
@@ -1,66 +1,104 @@
 package xyz.ottr.lutra.cli;
 
+/*-
+ * #%L
+ * xyz.ottr.lutra:lutra-cli
+ * %%
+ * Copyright (C) 2018 - 2022 University of Oslo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
+import xyz.ottr.lutra.system.Assertions;
+import xyz.ottr.lutra.system.Message;
 import xyz.ottr.lutra.system.MessageHandler;
-import xyz.ottr.lutra.wottr.io.RDFIO;
+
 
 public class Issue324DummyInput {
 
     private static final String ROOT = "src/test/resources/issues/324DummyInput/";
 
     @Test
-    public void parseDummyFile() {
-
+    public void parseCorrectFile() {
         String args = " "
-                + " --debugStackTrace"
-                + " -l " + ROOT + "dummy.ttl"
-                + " " + ROOT + "dummy.ttl";
+                + " -l " + ROOT + "templates/personTemplate.stottr"
+                + " --stdout"
+                + " " + ROOT + "instances/correct.ttl";
+
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        cli.executeArgs(args.trim().split("\\s+"));
+
+        Assertions.noErrors(msgs);
+    }
+
+    @Test
+    public void parseDummyFile() {
+        String args = " "
+                + " -l " + ROOT + "templates/personTemplate.stottr"
+                + " " + ROOT + "instances/dummy.ttl";
 
         CLI cli = new CLI();
         MessageHandler msgs = cli.getMessageHandler();
         int exitCode = cli.executeArgs(args.trim().split("\\s+"));
 
-        //assertNotEquals(0, msgs.getMessages().size());
-        //assertEquals(Message.Severity.ERROR, msgs.getMostSevere());
-        //assertNotEquals("Exit code should not be 0 with error messages", 0, exitCode);
-
-
-        //var result = RDFIO.fileReader().parse(ROOT + "dummy.ttl");
-        //System.out.println(result);
+        assertNotEquals(0, msgs.getMessages().size());
+        assertNotEquals("Exit code should not be 0 with error messages", 0, exitCode);
     }
 
     @Test
     public void parseFaultyFile() {
-
         String args = " "
-                + " --debugStackTrace"
-                + " -l " + ROOT + "faultyRDF.ttl"
-                + " " + ROOT + "faultyRDF.ttl";
+                + " -l " + ROOT + "templates/personTemplate.stottr"
+                + " " + ROOT + "instances/faulty.ttl";
 
         CLI cli = new CLI();
         MessageHandler msgs = cli.getMessageHandler();
         int exitCode = cli.executeArgs(args.trim().split("\\s+"));
-        //var result = RDFIO.fileReader().parse(ROOT + "faultyRDF.ttl");
-        //System.out.println(result);
+
+        assertNotEquals(0, msgs.getMessages().size());
+        assertNotEquals("Exit code should not be 0 with error messages", 0, exitCode);
     }
 
     @Test
-    public void parseFaultyFileRDFReader() {
+    public void readEmptyInstance() {
+        String args = " "
+                + " -l " + ROOT + "templates/personTemplate.stottr"
+                + " " + ROOT + "instances/empty.ttl";
 
-        var result = RDFIO.fileReader().parse(ROOT + "faultyRDF.ttl");
-        System.out.println(result);
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        cli.executeArgs(args.trim().split("\\s+"));
+
+        Assertions.atLeast(msgs, Message.Severity.WARNING);
     }
 
-
     @Test
-    public void parseEmptyFile() {
-        var result = RDFIO.fileReader().parse(ROOT + "empty.ttl");
-        System.out.println(result);
-    }
+    public void readEmptyTemplate() {
+        String args = " "
+                + " -l " + ROOT + "templates/emptyTemplate.stottr"
+                + " " + ROOT + "instances/correct.ttl";
 
-    @Test
-    public void parseCorrectFile() {
-        var result = RDFIO.fileReader().parse(ROOT + "correct.ttl");
-        System.out.println(result);
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        cli.executeArgs(args.trim().split("\\s+"));
+
+        Assertions.atLeast(msgs, Message.Severity.WARNING);
     }
 }
