b5add111c12a298841f7f83edb6d45a560abfec3
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-05-01 20:54:12 +0200

    Add new tests. #186

--- lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issues186Test.java
+++ lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issues186Test.java
@@ -26,19 +26,46 @@ import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import org.junit.Test;
+import xyz.ottr.lutra.system.Assertions;
+import xyz.ottr.lutra.system.Message;
+import xyz.ottr.lutra.system.MessageHandler;
 
 public class Issues186Test {
 
     private static final String ROOT = "src/test/resources/issues/186/";
     
     @Test
-    public void test() {
-        CLIRunner.run(
-            "-m expand -l " + ROOT + "/lib -L stottr -f -o " + ROOT + "out.ttl -O wottr -I tabottr " + ROOT + "instances.xlsx");
+    public void test_compatible_types() {
+        String args = " -f "
+                + " -l " + ROOT + "/lib"
+                + " -L stottr -I tabottr"
+                + " " + ROOT + "instances.xlsx"
+                + " -o " + ROOT + "out.ttl"
+                + " -O wottr";
+
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        cli.executeArgs(args.trim().split("\\s+"));
 
         assertTrue("Output file not found.", Files.isRegularFile(Paths.get(ROOT + "out.ttl")));
+        Assertions.noErrors(msgs);
+    }
+
+    @Test
+    public void test_incompatible_types() {
+        String args = " -f "
+                + " -l " + ROOT + "/lib_incompatible_types"
+                + " -L stottr -I tabottr"
+                + " " + ROOT + "instances.xlsx"
+                + " -o " + ROOT + "out.ttl"
+                + " -O wottr";
+
+        CLI cli = new CLI();
+        MessageHandler msgs = cli.getMessageHandler();
+        cli.executeArgs(args.trim().split("\\s+"));
+
+        Assertions.atLeast(msgs, Message.Severity.ERROR);
     }
 
 }
