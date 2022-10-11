763ae3c5c1d93927e00f28189238972b7e51f214
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-10-11 13:13:08 +0200

    Compare result model with original #242

--- lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issue242FormatLibraryTest.java
+++ lutra-cli/src/test/java/xyz/ottr/lutra/cli/Issue242FormatLibraryTest.java
@@ -1,7 +1,10 @@
 package xyz.ottr.lutra.cli;
 
+import org.apache.jena.rdf.model.Model;
 import org.junit.Test;
+import xyz.ottr.lutra.system.Assertions;
 import xyz.ottr.lutra.system.MessageHandler;
+import xyz.ottr.lutra.wottr.io.RDFIO;
 
 public class Issue242FormatLibraryTest {
 
@@ -12,17 +15,28 @@ public class Issue242FormatLibraryTest {
         CLI cli = new CLI();
         MessageHandler msgs = cli.getMessageHandler();
 
+        // format wottr to stottr
         String args = "-m formatLibrary -L wottr -O stottr "
                 + " -l " + ROOT + "templates.wottr "
                 + "-o " + ROOT + "out";
 
         cli.executeArgs(args.trim().split("\\s+"));
+        Assertions.noErrors(msgs);
 
+        // format stottr back to wottr
         String args2 = "-m formatLibrary -L stottr -O wottr "
                 + " -l " + ROOT + "out/example.com/ns/Person.stottr "
                 + "-o " + ROOT + "out";
 
         cli.executeArgs(args2.trim().split("\\s+"));
+        Assertions.noErrors(msgs);
+
+        // compare result with original
+        Model actual = RDFIO.fileReader().parse(ROOT + "out/example.com/ns/Person.ttl").get();
+        Model expected = RDFIO.fileReader().parse(ROOT + "templates.wottr").get();
+
+        // original template missing parameter type
+        TestUtils.testIsomorphicModels(actual, expected);
 
     }
 
